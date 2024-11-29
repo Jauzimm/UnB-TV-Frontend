1d116afc783ea6d356760ff52e88e23e808b047a
Jauzimm <joaovitorso071@gmail.com>
2024-11-29 16:05:14 -0300

    Correção das rotas

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  // videoAPIURL: process.env["videoAPIURL"],
-  videoAPIURL: "https://unbtv-videoservice.vercel.app/api",
-  // usersAPIURL: process.env["usersAPIURL"],
-  usersAPIURL: "https://unbtv.vercel.app/api",
-  // adminAPIURL: process.env["adminAPIURL"],
-  adminAPIURL: "https://unbtv-adminservice.vercel.app/api",
+  videoAPIURL: process.env["videoAPIURL"],
+  //videoAPIURL: "https://unbtv-videoservice.vercel.app/api",
+  usersAPIURL: process.env["usersAPIURL"],
+  //usersAPIURL: "https://unbtv.vercel.app/api",
+  adminAPIURL: process.env["adminAPIURL"],
+  //adminAPIURL: "https://unbtv-adminservice.vercel.app/api",
   EDUPLAY_CLIENT_KEY: process.env["EDUPLAY_CLIENT_KEY"] as string,
 };
